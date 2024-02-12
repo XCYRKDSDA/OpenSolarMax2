32259565d97c821636442cba6a346d9f19672269
XCYRKDSDA <personalXCY@outlook.com>
2024-02-12 23:21:44 +0800

    实现公转组件与系统

---
+++ OpenSolarMax.Core/Components/Revolution.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Nine.Drawing;
+
+namespace OpenSolarMax.Core.Components;
+
+/// <summary>
+/// 公转的模式
+/// </summary>
+public enum RevolutionMode
+{
+    TranslationAndRotation, //同时进行平动和转动
+    TranslationOnly,        //只进行平动, 不进行转动
+}
+
+/// <summary>
+/// 轨道组件。描述当前实体正在运行的轨道
+/// </summary>
+public struct RevolutionOrbit()
+{
+    /// <summary>
+    /// 轨道旋转。描述轨道圆所在坐标系相对星球坐标系的旋转
+    /// </summary>
+    public Quaternion Rotation = Quaternion.Identity;
+
+    /// <summary>
+    /// 轨道作为一个椭圆的横纵尺寸
+    /// </summary>
+    public SizeF Shape;
+
+    /// <summary>
+    /// 轨道上星球公转周期
+    /// </summary>
+    public float Period;
+
+    /// <summary>
+    /// 按当前轨道转动的模式
+    /// </summary>
+    public RevolutionMode Mode;
+}
+
+/// <summary>
+/// 公转状态组件。描述当前实体绕其当前轨道公转的实时状态
+/// </summary>
+public struct RevolutionState
+{
+    /// <summary>
+    /// 当前相位
+    /// </summary>
+    public float Phase;
+}
---
+++ OpenSolarMax.Core/Systems/RevolveEntitiesAroundOrbitsSystem.cs
@@ -0,0 +1,49 @@
+﻿using Arch.Core;
+using Arch.System;
+using Arch.System.SourceGenerator;
+using Microsoft.Xna.Framework;
+using OpenSolarMax.Core.Components;
+
+namespace OpenSolarMax.Core.Systems;
+
+/// <summary>
+/// 处理实体绕其轨道公转的系统
+/// </summary>
+internal sealed partial class RevolveEntitiesAroundOrbitsSystem(World world) : BaseSystem<World, GameTime>(world)
+{
+    /// <summary>
+    /// 计算实体绕其所在轨道公转的相对位姿
+    /// </summary>
+    /// <param name="orbit">实体所在轨道</param>
+    /// <param name="state">实体当前公转状态</param>
+    /// <returns>单位相对轨道所在实体的相对变换</returns>
+    private static RelativeTransform CalculateRevolutionTransform(in RevolutionOrbit orbit, in RevolutionState state) =>
+        // 以+Z轴为轴, 逆时针旋转
+        new(Matrix.CreateTranslation(orbit.Shape.Width, 0, 0)
+            * Matrix.CreateRotationZ(state.Phase)
+            * Matrix.CreateScale(1, orbit.Shape.Height / orbit.Shape.Width, 1)
+            * Matrix.CreateFromQuaternion(orbit.Rotation));
+
+    [Query]
+    [All<TreeRelationship<RelativeTransform>, RelativeTransform, RevolutionOrbit, RevolutionState>]
+    private static void UpdateRevolution([Data] GameTime time, in RevolutionOrbit orbit, ref RevolutionState state, ref RelativeTransform transform)
+    {
+        // 更新旋转状态
+        state.Phase += MathF.PI * 2 * (float)time.ElapsedGameTime.TotalSeconds / orbit.Period;
+
+        // 更新相对位姿
+        switch (orbit.Mode)
+        {
+            case RevolutionMode.TranslationAndRotation:
+                transform = CalculateRevolutionTransform(in orbit, in state);
+                break;
+
+            case RevolutionMode.TranslationOnly:
+                transform.Translation = CalculateRevolutionTransform(in orbit, in state).Translation;
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
