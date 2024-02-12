3ec16412d3e60318bd3efa0105002739f7271172
XCYRKDSDA <personalXCY@outlook.com>
2024-02-12 18:49:21 +0800

    初步提供了星球和相机的原型

---
+++ OpenSolarMax.Core/Archetypes.cs
@@ -0,0 +1,21 @@
+﻿using OpenSolarMax.Core.Components;
+using OpenSolarMax.Core.Utils;
+
+namespace OpenSolarMax.Core;
+
+public static class Archetypes
+{
+    public static readonly Archetype Planet = new(
+        typeof(AbsoluteTransform),
+        typeof(RelativeTransform),
+        typeof(TreeRelationship<RelativeTransform>),
+        typeof(Sprite)
+    );
+
+    public static readonly Archetype Camera = new(
+        typeof(AbsoluteTransform),
+        typeof(RelativeTransform),
+        typeof(TreeRelationship<RelativeTransform>),
+        typeof(Camera)
+    );
+}
