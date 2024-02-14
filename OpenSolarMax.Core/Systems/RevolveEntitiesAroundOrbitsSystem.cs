f3a11612b7e7bbe743b6a0fab7fb129dee3257ce
XCYRKDSDA <personalXCY@outlook.com>
2024-02-14 10:31:09 +0800

    将需要复用的静态方法暴露出来

--- OpenSolarMax.Core/Systems/RevolveEntitiesAroundOrbitsSystem.cs
+++ OpenSolarMax.Core/Systems/RevolveEntitiesAroundOrbitsSystem.cs
@@ -3,6 +3,7 @@ using Arch.System;
 using Arch.System.SourceGenerator;
 using Microsoft.Xna.Framework;
 using OpenSolarMax.Core.Components;
+using OpenSolarMax.Core.Utils;
 
 namespace OpenSolarMax.Core.Systems;
 
@@ -11,19 +12,6 @@ namespace OpenSolarMax.Core.Systems;
 /// </summary>
 public sealed partial class RevolveEntitiesAroundOrbitsSystem(World world) : BaseSystem<World, GameTime>(world)
 {
-    /// <summary>
-    /// 计算实体绕其所在轨道公转的相对位姿
-    /// </summary>
-    /// <param name="orbit">实体所在轨道</param>
-    /// <param name="state">实体当前公转状态</param>
-    /// <returns>单位相对轨道所在实体的相对变换</returns>
-    private static RelativeTransform CalculateRevolutionTransform(in RevolutionOrbit orbit, in RevolutionState state) =>
-        // 以+Z轴为轴, 逆时针旋转
-        new(Matrix.CreateTranslation(orbit.Shape.Width, 0, 0)
-            * Matrix.CreateRotationZ(state.Phase)
-            * Matrix.CreateScale(1, orbit.Shape.Height / orbit.Shape.Width, 1)
-            * Matrix.CreateFromQuaternion(orbit.Rotation));
-
     [Query]
     [All<TreeRelationship<RelativeTransform>, RelativeTransform, RevolutionOrbit, RevolutionState>]
     private static void UpdateRevolution([Data] GameTime time, in RevolutionOrbit orbit, ref RevolutionState state, ref RelativeTransform transform)
@@ -35,11 +23,11 @@ public sealed partial class RevolveEntitiesAroundOrbitsSystem(World world) : Bas
         switch (orbit.Mode)
         {
             case RevolutionMode.TranslationAndRotation:
-                transform = CalculateRevolutionTransform(in orbit, in state);
+                transform = Revolution.CalculateTransform(in orbit, in state);
                 break;
 
             case RevolutionMode.TranslationOnly:
-                transform.Translation = CalculateRevolutionTransform(in orbit, in state).Translation;
+                transform.Translation = Revolution.CalculateTransform(in orbit, in state).Translation;
                 break;
 
             default:
---
+++ OpenSolarMax.Core/Utils/Revolution.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using OpenSolarMax.Core.Components;
+
+namespace OpenSolarMax.Core.Utils;
+public static class Revolution
+{
+    /// <summary>
+    /// 计算实体绕其所在轨道公转的相对位姿
+    /// </summary>
+    /// <param name="orbit">实体所在轨道</param>
+    /// <param name="state">实体当前公转状态</param>
+    /// <returns>单位相对轨道所在实体的相对变换</returns>
+    public static RelativeTransform CalculateTransform(in RevolutionOrbit orbit, in RevolutionState state) =>
+        // 以+Z轴为轴, 逆时针旋转
+        new(Matrix.CreateTranslation(orbit.Shape.Width, 0, 0)
+            * Matrix.CreateRotationZ(state.Phase)
+            * Matrix.CreateScale(1, orbit.Shape.Height / orbit.Shape.Width, 1)
+            * Matrix.CreateFromQuaternion(orbit.Rotation));
+}
