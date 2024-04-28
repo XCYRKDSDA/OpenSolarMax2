b77b8e5b60607c14e166c142a06831fcc860809d
XCYRKDSDA <personalXCY@outlook.com>
2024-04-28 12:14:52 +0800

    实现单位抵达目的地后锚定到星球

--- OpenSolarMax.Mods.Core/Systems/ShipSystem.cs
+++ OpenSolarMax.Mods.Core/Systems/ShipSystem.cs
@@ -1,10 +1,12 @@
 ﻿using Arch.Core;
+using Arch.Core.Extensions;
 using Arch.System;
 using Arch.System.SourceGenerator;
 using Microsoft.Xna.Framework;
 using Nine.Assets;
 using OpenSolarMax.Game.ECS;
 using OpenSolarMax.Mods.Core.Components;
+using OpenSolarMax.Mods.Core.Utils;
 
 namespace OpenSolarMax.Mods.Core.Systems;
 
@@ -29,11 +31,44 @@ public sealed partial class UpdateShipStateSystem(World world, IAssetsManager as
 public sealed partial class ShipSystem(World world, IAssetsManager assets)
     : BaseSystem<World, GameTime>(world), ISystem
 {
+    private readonly List<Entity> _shipsArrived = [];
+
     [Query]
     [All<ShippingTask, ShippingState, RelativeTransform>]
-    private static void CalculatePosition(in ShippingTask task, in ShippingState state, ref RelativeTransform pose)
+    private static void CheckProgress(Entity ship, in ShippingTask task, in ShippingState state, ref RelativeTransform pose,
+                                      [Data] List<Entity> shipsArrived)
     {
         var progress = state.TravelledTime / task.ExpectedTravelDuration;
-        pose.Translation = Vector3.Lerp(task.DeparturePosition, task.ExpectedArrivalPosition, progress);
+
+        if (progress >= 1)
+        {
+            // 记录已抵达的单位
+            shipsArrived.Add(ship);
+        }
+        else
+        {
+            // 更新位置
+            pose.Translation = Vector3.Lerp(task.DeparturePosition, task.ExpectedArrivalPosition, progress);
+        }
+    }
+
+    private static void FinishShipping(IReadOnlyList<Entity> shipsArrived)
+    {
+        foreach (var ship in shipsArrived)
+        {
+            var task = ship.Get<ShippingTask>();
+
+            // 将单位挂载到目标星球
+            AnchorageUtils.JustAnchorTo(ship, task.DestinationPlanet);
+            ship.Add(task.ExpectedRevolutionOrbit, task.ExpectedRevolutionState);
+            ship.Remove<ShippingTask, ShippingState>();
+        }
+    }
+
+    public override void Update(in GameTime t)
+    {
+        CheckProgressQuery(World, _shipsArrived);
+        FinishShipping(_shipsArrived);
+        _shipsArrived.Clear();
     }
 }
--- OpenSolarMax.Mods.Core/Utils/AnchorageUtils.cs
+++ OpenSolarMax.Mods.Core/Utils/AnchorageUtils.cs
@@ -1,5 +1,6 @@
 ﻿using Arch.Core;
 using Arch.Core.Extensions;
+using Microsoft.Xna.Framework;
 using OpenSolarMax.Mods.Core.Components;
 
 namespace OpenSolarMax.Mods.Core.Utils;
@@ -9,7 +10,7 @@ public static class AnchorageUtils
     private const float _defaultRevolutionOffsetRange = 0.3f;
 
     public static void AnchorTo(this Entity ship, Entity planet, Random? random = null,
-                                float revolutionOffsetRange = _defaultRevolutionOffsetRange)
+                                        float revolutionOffsetRange = _defaultRevolutionOffsetRange)
     {
         random ??= new();
 
@@ -23,5 +24,56 @@ public static class AnchorageUtils
         ship.Get<RevolutionOrbit>() = RevolutionUtils.CreateRandomRevolutionOrbit(
             in planet.Get<PlanetGeostationaryOrbit>(), random, revolutionOffsetRange);
         ship.Get<RevolutionState>() = RevolutionUtils.CreateRandomState(random);
+    }
+
+    //public static void VisuallyAnchorTo(this Entity ship, Entity planet, in Matrix view)
+    public static void JustAnchorTo(this Entity ship, Entity planet)
+    {
+        // 设置停靠关系
+        ship.SetParent<Anchorage>(planet);
+
+        // 设置变换关系
+        ship.SetParent<RelativeTransform>(planet);
+
+        //// 根据当前位置计算视觉上合适的公转轨道
+
+        //// 计算星球轨道平面
+        //ref readonly var planetPose = ref planet.Get<AbsoluteTransform>();
+        //ref readonly var planetOrbit = ref planet.Get<PlanetGeostationaryOrbit>();
+        //var planetOrbitPlane = (
+        //    Position: planetPose.Translation,
+        //    Normal: Vector3.TransformNormal(Vector3.UnitZ, Matrix.CreateFromQuaternion(planetOrbit.Rotation * planetPose.Rotation))
+        //);
+
+        //// 计算单位位置
+        //var shipPosition = ship.Get<AbsoluteTransform>().Translation;
+
+        //// 将单位位置反向投射到星球的轨道平面
+        //var shipPositionProjection = (
+        //    Position: shipPosition,
+        //    Direction: Vector3.TransformNormal(Vector3.UnitZ, Matrix.Invert(view))
+        //);
+        //var projectedShipPosition =
+        //    shipPositionProjection.Position
+        //    + shipPositionProjection.Direction
+        //      * (Vector3.Dot(planetOrbitPlane.Position - shipPositionProjection.Position, planetOrbitPlane.Normal)
+        //         / Vector3.Dot(shipPositionProjection.Direction, planetOrbitPlane.Normal));
+
+        //// 设置位置，并计算单位的轨道
+        //ship.Get<AbsoluteTransform>().Translation = projectedShipPosition;
+        //var radius = (projectedShipPosition - planetPose.Translation).Length();
+    }
+
+    public static void UnanchorTo(this Entity ship, Entity planet)
+    {
+        // 解除停靠关系
+        ship.RemoveParent<Anchorage>();
+
+        // 解除变换树关系，但是保持当前绝对位姿
+        ship.RemoveParent<RelativeTransform>();
+        ship.Get<RelativeTransform>().TransformToParent = ship.Get<AbsoluteTransform>().TransformToRoot;
+
+        // 移除轨道
+        ship.Remove<RevolutionOrbit, RevolutionState>();
     }
 }
