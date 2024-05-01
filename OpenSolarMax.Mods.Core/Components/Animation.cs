9b2d42b26d2399065ffbae256978fda3b2e6c75b
XCYRKDSDA <personalXCY@outlook.com>
2024-05-01 18:21:55 +0800

    支持动画过渡

--- OpenSolarMax.Mods.Core/Components/Animation.cs
+++ OpenSolarMax.Mods.Core/Components/Animation.cs
@@ -4,6 +4,17 @@ using OpenSolarMax.Game.ECS;
 
 namespace OpenSolarMax.Mods.Core.Components;
 
+public struct Transition
+{
+    public AnimationClip<Entity>? PreviousClip;
+
+    public float PreviousClipTime;
+
+    public float Duration;
+
+    public ICurve<float>? Tweener;
+}
+
 /// <summary>
 /// 动画组件。描述当前实体正在播放的动画剪辑和时间
 /// </summary>
@@ -13,4 +24,6 @@ public struct Animation
     public AnimationClip<Entity>? Clip;
 
     public float LocalTime;
+
+    public Transition? Transition;
 }
--- OpenSolarMax.Mods.Core/Systems/AnimateSystems.cs
+++ OpenSolarMax.Mods.Core/Systems/AnimateSystems.cs
@@ -17,13 +17,17 @@ public sealed partial class UpdateAnimationTimeSystem(World world, IAssetsManage
     [All<Animation>]
     private static void Animate([Data] GameTime t, ref Animation animation)
     {
-        if (animation.Clip is null)
+        if (animation.Clip is null && animation.Transition is null)
             return;
 
         animation.LocalTime += (float)t.ElapsedGameTime.TotalSeconds;
 
-        // 将动画时间限制在四倍动画时长之内，保证float的精度，同时不论对于循环动画还是折返循环动画都能正常工作
-        if (animation.LocalTime >= animation.Clip.Length * 4)
+        // 当动画时间超过了过渡时间时，说明过渡已经完成，则不再记录上一则动画，并且允许对动画时间进行限幅
+        if (animation.Transition is not null && animation.LocalTime >= animation.Transition.Value.Duration)
+            animation.Transition = null;
+
+        // 当不存在过渡需求时，将动画时间限制在四倍动画时长之内，保证float的精度，同时不论对于循环动画还是折返循环动画都能正常工作
+        if (animation.Transition is null && animation.Clip is not null && animation.LocalTime >= animation.Clip.Length * 4)
             animation.LocalTime -= animation.Clip.Length * 2;
     }
 }
@@ -36,9 +40,16 @@ public sealed partial class AnimateSystem(World world, IAssetsManager assets)
     [All<Animation>]
     private static void Animate(Entity entity, in Animation animation)
     {
-        if (animation.Clip is null)
-            return;
-
-        AnimationEvaluator<Entity>.EvaluateAndSet(ref entity, animation.Clip, animation.LocalTime);
+        if (animation.Transition is null)
+            AnimationEvaluator<Entity>.EvaluateAndSet(ref entity, animation.Clip, animation.LocalTime);
+        else
+        {
+            AnimationEvaluator<Entity>.TweenAndSet(
+                ref entity,
+                animation.Transition.Value.PreviousClip, animation.Transition.Value.PreviousClipTime + animation.LocalTime,
+                animation.Clip, animation.LocalTime,
+                animation.Transition.Value.Tweener, animation.LocalTime / animation.Transition.Value.Duration
+            );
+        }
     }
 }
