14f1a8441e0f951731c021cbda93ffdc6679ce96
XCYRKDSDA <personalXCY@outlook.com>
2025-03-04 00:47:57 +0800

    feat(ci): show warp pulse when ships charge and arrive
    and tune animations according to SolarMax2

---
+++ OpenSolarMax.Mods.Core/Systems/Transportation/StartTransportationSystem.cs
@@ -0,0 +1,101 @@
+using System.Diagnostics;
+using Arch.Buffer;
+using Arch.Core;
+using Arch.Core.Extensions;
+using Arch.System;
+using Arch.System.SourceGenerator;
+using Microsoft.Xna.Framework;
+using Nine.Assets;
+using OpenSolarMax.Game.ECS;
+using OpenSolarMax.Game.Modding;
+using OpenSolarMax.Game.Utils;
+using OpenSolarMax.Mods.Core.Components;
+using OpenSolarMax.Mods.Core.Templates;
+
+namespace OpenSolarMax.Mods.Core.Systems.Transportation;
+
+/// <summary>
+/// 处理<see cref="StartShippingRequest"/>使传送门上单位开始传送的系统
+/// </summary>
+[StructuralChangeSystem]
+[ExecuteBefore(typeof(StartShippingSystem))]
+public sealed partial class StartTransportationSystem(World world, IAssetsManager assets)
+    : BaseSystem<World, GameTime>(world), ISystem
+{
+    private readonly CommandBuffer _commandBuffer = new();
+
+    public void ModifyOthers(ISystemProvider systems)
+    {
+        systems.Get<HandleInputsOnManeuveringShipsSystem>().ReachabilityCheckers.Add(
+            (_, departure, _) => departure.Has<PortalChargingJobs>()
+        );
+        systems.Get<VisualizeManeuveringShipsStatusSystem>().ReachabilityCheckers.Add(
+            (_, departure, _) => departure.Has<PortalChargingJobs>()
+        );
+    }
+
+    [Query]
+    [All<StartShippingRequest>]
+    private void StartTransporting(Entity requestEntity, in StartShippingRequest request)
+    {
+        Debug.Assert(requestEntity.WorldId == request.Departure.Entity.WorldId
+                     && requestEntity.WorldId == request.Destination.Entity.WorldId
+                     && requestEntity.WorldId == request.Party.Entity.WorldId);
+
+        if (!request.Departure.Entity.Has<PortalChargingJobs>())
+            return;
+
+        // 设置单位传送状态
+        var shipsRemain = request.ExpectedNum;
+        var allShips = request.Departure.Entity.Get<AnchoredShipsRegistry>().Ships[request.Party];
+        using var shipsEnumerator = allShips.GetEnumerator();
+        while (shipsRemain > 0 && shipsEnumerator.MoveNext())
+        {
+            var ship = shipsEnumerator.Current;
+
+            // 获取相关信息
+            var transformRelationship =
+                ship.Entity.Get<TreeRelationship<RelativeTransform>.AsChild>().Relationship!.Value.Ref;
+            ref readonly var revolutionOrbit = ref transformRelationship.Entity.Get<RevolutionOrbit>();
+            ref readonly var revolutionState = ref transformRelationship.Entity.Get<RevolutionState>();
+            ref readonly var departurePlanetOrbit = ref request.Departure.Entity.Get<PlanetGeostationaryOrbit>();
+            ref readonly var destinationPlanetOrbit = ref request.Destination.Entity.Get<PlanetGeostationaryOrbit>();
+
+            // 计算泊入轨道
+            var orbitOffset = revolutionOrbit.Shape.X / 2 / departurePlanetOrbit.Radius;
+            var expectedOrbit = new RevolutionOrbit()
+            {
+                Rotation = destinationPlanetOrbit.Rotation,
+                Shape = new(destinationPlanetOrbit.Radius * orbitOffset * 2,
+                            destinationPlanetOrbit.Radius * orbitOffset * 2),
+                Period = destinationPlanetOrbit.Period * MathF.Pow(orbitOffset, 1.5f)
+            };
+
+            ref var transportingStatus = ref ship.Entity.Get<TransportingStatus>();
+            transportingStatus.State = TransportingState.PreTransportation;
+            transportingStatus.Task = new()
+            {
+                DestinationPlanet = request.Destination,
+                ExpectedRevolutionOrbit = expectedOrbit,
+                ExpectedRevolutionState = revolutionState
+            };
+            transportingStatus.PreTransportation = new() { ElapsedTime = TimeSpan.Zero };
+        }
+
+        // 创建传送门特效
+        World.Make(new PortalChargingEffectTemplate(assets)
+        {
+            Portal = request.Departure,
+            PortalRadius = request.Departure.Entity.Get<ReferenceSize>().Radius,
+            Color = request.Party.Entity.Get<PartyReferenceColor>().Value
+        }).Reference();
+
+        _commandBuffer.Destroy(in requestEntity);
+    }
+
+    public override void Update(in GameTime t)
+    {
+        StartTransportingQuery(World);
+        _commandBuffer.Playback(World);
+    }
+}
