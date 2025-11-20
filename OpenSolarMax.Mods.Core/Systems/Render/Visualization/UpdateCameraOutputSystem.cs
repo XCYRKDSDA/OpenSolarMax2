71839a7681baea5cb870c4e3e111bae02f309485
XCYRKDSDA <personalXCY@outlook.com>
2025-11-20 13:21:23 +0800

    feat(rendering): calculate camera output from manally given viewport

--- OpenSolarMax.Mods.Core/Systems/Render/Visualization/UpdateCameraOutputSystem.cs
+++ OpenSolarMax.Mods.Core/Systems/Render/Visualization/UpdateCameraOutputSystem.cs
@@ -2,30 +2,30 @@ using Arch.Core;
 using Arch.System;
 using Arch.System.SourceGenerator;
 using Microsoft.Xna.Framework.Graphics;
-using OpenSolarMax.Game;
 using OpenSolarMax.Game.Modding;
 using OpenSolarMax.Mods.Core.Components;
 
 namespace OpenSolarMax.Mods.Core.Systems;
 
 [RenderSystem, AfterStructuralChanges]
-[Write(typeof(Camera))]
-// LevelUIContext 粒度太粗，此处不写
+[ReadCurr(typeof(Viewport)), Write(typeof(Camera))]
 public sealed partial class UpdateCameraOutputSystem(World world) : ICalcSystem
 {
-    [Query]
-    [All<Camera, LevelUIContext>]
-    private static void UpdateOutput(ref Camera camera, in LevelUIContext uiContext)
-    {
-        var worldBounds = uiContext.WorldPad.ContainerBounds;
-
-        var scaleX = worldBounds.Width / camera.Width;
-        var scaleY = worldBounds.Height / camera.Height;
-        var scale = MathF.Min(scaleX, scaleY);
-
-        camera.Output = new Viewport(worldBounds.X, worldBounds.Y,
-                                     (int)MathF.Round(scale * camera.Width), (int)MathF.Round(scale * camera.Height));
-    }
-
     public void Update() => UpdateOutputQuery(world);
+
+    [Query]
+    [All<Camera, Viewport>]
+    private static void UpdateOutput(ref Camera camera, in Viewport viewport)
+    {
+        // 计算缩放
+        var scaleX = viewport.Width / camera.Width;
+        var scaleY = viewport.Height / camera.Height;
+        var scale = MathF.Min(scaleX, scaleY);
+        camera.Output.Width = (int)MathF.Round(scale * camera.Width);
+        camera.Output.Height = (int)MathF.Round(scale * camera.Height);
+
+        // 居中
+        camera.Output.X = viewport.X + (viewport.Width - camera.Output.Width) / 2;
+        camera.Output.Y = viewport.Y + (viewport.Height - camera.Output.Height) / 2;
+    }
 }
