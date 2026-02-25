957b5843307cb34c4fb82ae7fb7b2567d4752170
XCYRKDSDA <personalXCY@outlook.com>
2026-02-25 23:29:50 +0800

    fix(configs): fix value binding and parsing

--- OpenSolarMax.Game/Modding/Configuration/ConfigurationHelper.cs
+++ OpenSolarMax.Game/Modding/Configuration/ConfigurationHelper.cs
@@ -5,5 +5,9 @@ namespace OpenSolarMax.Game.Modding.Configuration;
 public static class ConfigurationHelper
 {
     public static T RequireValue<T>(this IConfiguration configuration, string key)
-        => configuration.GetValue<T?>(key) ?? throw new ArgumentNullException();
+    {
+        var section = configuration.GetRequiredSection(key);
+        var value = section.Get<T?>();
+        return value ?? throw new ArgumentNullException();
+    }
 }
