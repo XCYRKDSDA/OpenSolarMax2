ad1cff7f8bd22634552017f13fbe115dce4ffe0e
XCYRKDSDA <personalXCY@outlook.com>
2026-09-16 23:57:55 +0800

    Revert "fix(simulate): 公转系统要同时更新位置和姿态"
    This reverts commit cf06f22a9033af5a89e14fcacdec06af3c98318f.

--- src/OpenSolarMax.Mods.S2/Systems/Simulate/Revolution/CalculateTransformAroundOrbit.cs
+++ src/OpenSolarMax.Mods.S2/Systems/Simulate/Revolution/CalculateTransformAroundOrbit.cs
@@ -30,8 +30,13 @@ public sealed partial class CalculateTransformAroundOrbitSystem(World world) : I
         ref RelativeTransform transform
     )
     {
+        // 该系统同时负责天体绕轨道公转和飞船绕天体公转，而天体绕轨道公转是不应当改变姿态的。
+        // 虽然按理说飞船绕天体公转时应当改变姿态，但是游戏中也无处体现。之前以为不更新姿态影响了传送动画，实际上并非如此。
+        // 因此此处仍然只设置相对位置变换，不设置姿态。
+        // 后续若有需要，应当采用可配置的逻辑：是只更新位置，还是要更新姿态。
+
         // 更新相对位姿
-        transform = RevolutionUtils.CalculateTransform(in orbit, in state);
+        transform.Translation = RevolutionUtils.CalculateTransform(in orbit, in state).Translation;
     }
 
     public void Update() => CalculateTransformQuery(world);
